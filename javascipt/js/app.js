7c8f656190e1ce67d3a8766084be810d80a536c2
Jtruly <jayvel48@gmail.com>
2023-02-24 22:34:03 -0500

    2/24/23

---
+++ javascipt/js/app.js
@@ -0,0 +1,15 @@
+//Console log
+
+//console.log("YUURR")
+//alert("Stop")
+
+//document .getElementById("demo").style.color = "red"
+let demo = document .getElementById("demo");
+
+demo .innerHTML = "GreatGooglyMoogly";
+
+var demo = document .getElementById("AuntJemima");
+
+const demo = document .getElementById("HeyAuntie");
+
+let document .getElementById("BabySmoove") .style.color = "blue";
