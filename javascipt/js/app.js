fa5e5f67387ae1498a61bd812322d4afc814f624
Jtruly <jayvel48@gmail.com>
2023-02-28 23:16:07 -0500

    2/28/23

--- javascipt/js/app.js
+++ javascipt/js/app.js
@@ -1,11 +1,35 @@
-function greet () {
-    alert("hello")
-}
-//Calling function turn on washing machine
-greet()
-function square(number) 
-{return number * number;}
+function convertToDaysLeft(milliseconds) {
+    return Math.floor(milliseconds / (1000 * 60 * 60 * 24));
+    let daysLeft = convertToDaysLeft(timeleft)
+  }
 
-let result = square(1600)
-document.getElementById("demo")
-H
+  function convertToHoursLeft(milliseconds) {
+    return Math.floor(
+      (milliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let hoursLeft = convertToHoursLeft(timeleft)
+  }
+
+  function convertToMinutesLeft(milliseconds) {
+    return Math.floor(
+      (milliseconds % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    let minutesLeft = convertToMinutesLeft(timeleft) 
+  }
+
+  function convertToSecondsLeft(milliseconds) {
+    return Math.floor(
+      (milliseconds % (1000 * 60)) / 1000
+    );
+    let secondsLeft = convertToSecondsLeft(timeleft)
+  }
+  function setCountdown{
+    let now = new Date();
+    let timeleft = deadline.getTime() - now.getTime();
+  }
+
+  function formatCountdownText(days, hours, minutes, seconds) {
+    return `${days}d ${hours}h ${minutes}m ${seconds}s left`;
+    formatCountdownText(daysLeft, hoursLeft, minutesLeft, secondsLeft);
+  setInterval(setCountdown, 1000)
+}
