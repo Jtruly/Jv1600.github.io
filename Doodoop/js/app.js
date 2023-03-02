fd361560de846891abcbaf90b7e4f4476e5e56bc
Jtruly <jayvel48@gmail.com>
2023-03-01 22:49:09 -0500

    march

---
+++ Doodoop/js/app.js
@@ -0,0 +1,9 @@
+function displaydate() {
+    let demo = document.getElementById("demo")
+    demo.innerHTML = Date();
+} 
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener('click', displaydate);
+function
---
+++ js_calc/js/calc.js
@@ -0,0 +1,12 @@
+let answer = document.getElementById("answer");
+function twoPlusTwo() {
+    answer.innerHTML = 2 + 2;
+}
+function multiply(n1, n2) {
+    answer.innerHTML = n1 + n2;
+}
+function multiplyTwoNumbers() {
+    let firstNumber = document.getElementById("FirstNumber");
+    let secondNumber = document.getElementById("SecondNumber");
+
+}
