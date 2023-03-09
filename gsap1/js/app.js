0522d97a14a428894830c8493a1c49b032df817c
Jtruly <jayvel48@gmail.com>
2023-03-08 22:23:23 -0500

    mar8

---
+++ gsap1/js/app.js
@@ -0,0 +1,15 @@
+gsap.from(".chucked",{
+    duration: 5,
+    ease: "bounce",
+    rotation:720,
+    x: -500,
+    y: -500,
+    stagger: 0.5
+});
+
+gsap.from("headline",{
+    duration: 2.5,
+    opacity: 0,
+y: 300,
+stagger: 0.3,
+});
